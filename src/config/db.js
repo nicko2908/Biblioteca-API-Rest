6d5a25dea85b9692a2238b656e4229a841705715
nicko2908 <nickito2908@gmail.com>
2026-06-16 17:26:52 -0500

    Arreglo otra vez

--- src/config/db.js
+++ src/config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
         const conn = await mongoose.connect(process.env.MONGODB_URI);
         console.log(`MongoDB conectado: ${conn.connection.host}`);
     } catch (error) {
-        console.error(`Error al conectar: ${error.message}`);
+        console.error(`Error al conectar:`, error.stack); // <-- cambia esto
         process.exit(1);
     }
 };
